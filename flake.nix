de48db9b7587b1a88b3e41e80d0fe4ff066f1cc4
thein3rovert <danielolaibi@gmail.com>
2026-01-05 20:45:15 +0000

    chore(chore): create mkflake in addtion to mkLib

--- flake.nix
+++ flake.nix
@@ -30,8 +30,33 @@
         pkgs = packages;
       };
 
+      # Create flake option
+      mkFlake =
+        flake-and-lib-options@{
+          # What does the @ sign means?
+          inputs,
+          src,
+          arkadia ? { },
+          ...
+        }:
+        let
+          lib = mkLib {
+            # Q: What input is mkflake inherting? is it
+            # the custom or the main?
+            inherit inputs src arkadia;
+          };
+          # We remove attr inputs and src from the flake-option
+          # because we dont need them.
+          flake-options = builtins/removeAttrs flake-and-lib-options [
+            "inputs"
+            "src"
+          ];
+        in
+        lib.mkFlake flake-options;
     in
     {
+      inherit mkLib mkFlake;
+
       packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
       packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
