5b9efc60b85ccab119fa7e1dba312be3dbe47a55
thein3rovert <danielolaibi@gmail.com>
2026-01-05 20:21:42 +0000

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Arkadia Lib";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+
+    in
+    {
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+    };
+}
