ea6aeaac8a56c700fb467a8f8ba467b128658789
thein3rovert <danielolaibi@gmail.com>
2026-02-14 19:39:25 +0000

    chore: validate and check file type in a dir

---
+++ arkadia-lib/fs/default.nix
@@ -0,0 +1,42 @@
+{
+  core-inputs,
+  user-inputs,
+  arkadia-lib,
+  arkadia-config,
+}:
+let
+
+  inherit (builtins) readDir pathExists;
+  inherit (core-inputs) flake-utils-plus;
+  inherit (core-inputs.nixpkgs.lib)
+    assertMsg
+    filterAttrs
+    mapAttrsToList
+    flatten
+    ;
+
+  file-name-regex = "(.*)\\.(.*)$";
+in
+{
+
+  fs = rec {
+
+    # Check/validate if file type in a directory
+    ## Matchers for file kinds. These are often used with `readDir`.
+    ## Example Usage:
+    ## ```nix
+    ## is-file-kind "directory"
+    ## ```
+    ## Result:
+    ## ```nix
+    ## false
+    ## ```
+    ## ```
+    #@ String -> Bool
+    is-file-type = type: type == "regular";
+    is-symlink-type = type: type == "symlink";
+    is-directory-type = type: type == "directory";
+    is-unknown-tye = type: type == "unknown";
+
+  };
+}
